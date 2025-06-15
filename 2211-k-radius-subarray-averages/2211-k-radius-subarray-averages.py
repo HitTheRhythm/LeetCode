f0a84543e05e421ee1295bf2a3719e933edf722e
Jinyang Chen <61057655+HitTheRhythm@users.noreply.github.com>
2025-06-15 16:31:47 -0700

    Time: 59 ms (90.65%), Space: 31.8 MB (98.79%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.py
@@ -1,10 +1,10 @@
 class Solution:
     def getAverages(self, nums: List[int], k: int) -> List[int]:
+        if k == 0:
+            return nums
         n = len(nums)
         res = [-1] * n
-        if k == 0:
-            res = nums
-            return res
+        
         window = 0
         for i in range(k , n - k):
             if window == 0:
@@ -12,7 +12,6 @@ class Solution:
                 res[i] = window // (2 * k + 1)
                 continue
             window += nums[i + k] - nums[i - k -1]
-            print(f"{window} \n")
             res[i] = window // (2 * k + 1)
             
         return res
