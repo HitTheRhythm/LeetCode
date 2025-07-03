ac5031d2ac63e298bdd5c173fb1e2dd6ed2d6504
Jinyang Chen <61057655+HitTheRhythm@users.noreply.github.com>
2025-07-02 18:34:15 -0700

    Time: 0 ms (100%), Space: 17.8 MB (99.68%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -4,12 +4,9 @@ class Solution:
         
         while left < right:
             mid = (left + right) // 2
-            # 修正：比较 nums[mid] 和 nums[right]
             if nums[mid] > nums[right]:
-                # 最小值在右半部分
                 left = mid + 1
             else:
-                # 最小值在左半部分（包括mid）
                 right = mid
         
         return nums[left]
