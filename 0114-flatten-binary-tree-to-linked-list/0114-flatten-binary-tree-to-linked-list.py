25cbb507e7bafefa6f9be065aa2eecb418278e23
Jinyang Chen <61057655+HitTheRhythm@users.noreply.github.com>
2025-08-19 23:04:45 -0700

    Time: 0 ms (100%), Space: 18 MB (73.3%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -11,7 +11,7 @@ class Solution:
         """
         if not root:
             return
-
+        
         self.flatten(root.left)
         self.flatten(root.right)
         left = root.left
@@ -23,4 +23,28 @@ class Solution:
         while p.right:
             p = p.right
         p.right = right
+
+
+
+
+
+
+
+
+
+
+        # if not root:
+        #     return
+
+        # self.flatten(root.left)
+        # self.flatten(root.right)
+        # left = root.left
+        # right = root.right
+        # root.left = None
+        # root.right = left
+
+        # p = root
+        # while p.right:
+        #     p = p.right
+        # p.right = right
 
