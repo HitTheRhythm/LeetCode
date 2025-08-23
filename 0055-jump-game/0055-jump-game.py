f18f796e8636f41eee4754e382661a4c89b2f12a
Jinyang Chen <61057655+HitTheRhythm@users.noreply.github.com>
2025-08-22 19:52:11 -0700

    Time: 42 ms (30.42%), Space: 18.5 MB (85.9%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,11 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
-        farthest = 0
-        for i in range(n - 1):
-            # 不断计算能跳到的最远距离
-            farthest = max(farthest, i + nums[i])
-            # 可能碰到了 0，卡住跳不动了
-            if farthest <= i:
+        cover = 0
+
+        for i in range(n - 1 ):
+            cover = max(cover, i + nums[i])
+            if cover <= i:
                 return False
-        return farthest >= n - 1
+        return cover >= n - 1
